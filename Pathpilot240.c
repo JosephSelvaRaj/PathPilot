d9e9fd1fec90e862cab093262f05456a70c9cc49
Joseph Rapz <2102883@sit.singaporetech.edu.sg>
2024-06-13 17:17:47 +0800

    Pathpilot max distance

--- Pathpilot240.c
+++ Pathpilot240.c
@@ -14,6 +14,7 @@
 // Macros
 #define MOTOR_STRAIGHT_SPEED 90
 #define LIDAR_RESOLUTION 240
+#define DISTANCE_MAX_THRESHOLD 2000
 #define LIDAR_SPEED 255
 #define LEFT_MOTOR_TUNE_DOWN_PERCENTAGE 0.97
 #define MOTOR_TURNING_RATIO 0.73
@@ -23,12 +24,12 @@
 RPLidar lidar;
 
 Eloquent::ML::Port::RandomForest clf;
-int lidarDataSelection[NUM_OF_FEATURES] = {46, 47, 48, 50, 51, 53, 54, 100, 102, 103, 104, 107, 132, 207,
-                                           209, 211, 212, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234,
-                                           235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248,
-                                           249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 301, 302, 303, 306,
-                                           307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320,
-                                           321, 322, 323, 324, 325, 326, 327, 328, 329, 330};
+int lidarDataSelection[NUM_OF_FEATURES] = {66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+                                           135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148,
+                                           149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162,
+                                           163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176,
+                                           177, 178, 179, 180, 181, 182, 184, 186, 200, 202, 204, 205, 206, 207,
+                                           208, 209, 210, 211, 212, 213, 214, 215, 216, 218};
 float selectedData[NUM_OF_FEATURES];
 int distanceBuffer[LIDAR_RESOLUTION];
 int controlCmd;
@@ -137,7 +138,7 @@ void processLidarData()
     distanceValue = (int)lidar.getCurrentPoint().distance;
     angleValue = (int)lidar.getCurrentPoint().angle;
     qualityValue = (int)lidar.getCurrentPoint().quality;
-    if (qualityValue > 0)
+    if (distanceValue <= DISTANCE_MAX_THRESHOLD && qualityValue > 0)
     {
         // Get the buffer index for the angle value
         int bufferIndex = angleIndexMap(angleValue);
