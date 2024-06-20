fa015a018d9b6f2925e48d1ac3f75d2367c25afa
Joseph Rapz <2102883@sit.singaporetech.edu.sg>
2024-06-20 13:15:38 +0800

    Files rename

--- Pathpilot.c
+++ Pathpilot.c
@@ -24,12 +24,7 @@
 RPLidar lidar;
 
 Eloquent::ML::Port::RandomForest clf;
-int lidarDataSelection[NUM_OF_FEATURES] = {66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-                                           135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148,
-                                           149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162,
-                                           163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176,
-                                           177, 178, 179, 180, 181, 182, 184, 186, 200, 202, 204, 205, 206, 207,
-                                           208, 209, 210, 211, 212, 213, 214, 215, 216, 218};
+int lidarDataSelection[NUM_OF_FEATURES] = {c};
 float selectedData[NUM_OF_FEATURES];
 int distanceBuffer[LIDAR_RESOLUTION];
 int controlCmd;
