461615cfcba6245044bc2b361ae148c695bcfd6e
Joseph Rapz <2102883@sit.singaporetech.edu.sg>
2024-06-20 13:27:54 +0800

    Trainers for MLP & decision Tree, and no K best

--- visualizer.py
+++ visualizer.py
@@ -5,7 +5,7 @@ pygame.init()
 
 # Constants
 LIDAR_RESOLUTION = 240
-MAX_DISTANCE = 3000  # You can adjust this based on the maximum distance expected in your data
+MAX_DISTANCE = 2000  # You can adjust this based on the maximum distance expected in your data
 SCREEN_SIZE = 800
 CENTER = SCREEN_SIZE // 2
 BACKGROUND_COLOR = (250, 250, 250)
@@ -16,15 +16,15 @@ ROBOT_RADIUS = 15
 HIGHLIGHTED_RADIUS = 4
 DEFAULT_RADIUS = 3
 DATA_FILE_PATH = 'C:\\Users\\josep\\Documents\\Github Repo\\PathPilot\\PathPilot\\MASTERDATA240CIRCLE.txt'
-WAIT_TIME = 50  # milliseconds
+WAIT_TIME = 200  # milliseconds
 
 highlighted_indices = [
-    66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
-       135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148,
-       149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162,
-       163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176,
-       177, 178, 179, 180, 181, 182, 184, 186, 200, 202, 204, 205, 206, 207,
-       208, 209, 210, 211, 212, 213, 214, 215, 216, 218
+    136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149,
+       150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163,
+       164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 176, 184, 185,
+       186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199,
+       200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213,
+       214, 215, 216, 217, 218, 219, 220, 221, 222, 223
 ]
 
 # Functions
